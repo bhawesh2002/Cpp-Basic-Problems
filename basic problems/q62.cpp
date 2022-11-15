6c75f41ad57712475e31bc1d06d828a374342a32
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-11-15 22:18:14 +0530

    comment the code

--- basic problems/q62.cpp
+++ basic problems/q62.cpp
@@ -7,22 +7,22 @@ In other words, a twin prime is a prime that has a prime gap of two".*/
 
 using namespace std;
 
-int primeCheck(int num)
+int primeCheck(int num) // function to check weather number is prime or not
 {
-    int check = true;
-    for (int i = 2; i < num; i++)
+    int check = true;             // set default value to true
+    for (int i = 2; i < num; i++) // run a loop
     {
-        if (num % i == 0)
+        if (num % i == 0) // check weather num is completely divisible by i
         {
-            check = false;
-            break;
+            check = false; // set value to false
+            break;         // break the loop
         }
     }
-    if (check == true)
+    if (check == true) // if default value is not changed number is prime and return 1
     {
         return 1;
     }
-    return 0;
+    return 0; // return 0 is value of check is false
 }
 
 int main()
@@ -30,23 +30,23 @@ int main()
     int num{};
     cout << "Enter a number: ";
     cin >> num;
-    int isPrime = primeCheck(num);
-    if (isPrime == 1)
+    int isPrime = primeCheck(num); // check weather num entered is prime or not
+    if (isPrime == 1)              // if yes
     {
-        int next = num + 2;
-        int isTwinPrime = primeCheck(next);
-        if (isPrime == isTwinPrime)
+        int next = num + 2;                 // add two to the prime number
+        int isTwinPrime = primeCheck(next); // check weather the new number is prime or not
+        if (isPrime == isTwinPrime)         // if yes
         {
-            cout << "Twin prime of " << num << " is " << next << endl;
+            cout << "Twin prime of " << num << " is " << next << endl; // the number is twin prime
         }
-        else
+        else // if no
         {
-            cout << num << " does not have a twin prime." << endl;
+            cout << num << " does not have a twin prime." << endl; // number is not a twin prime
         }
     }
-    else
+    else // if no
     {
-        cout << num << " is not a prime number." << endl;
+        cout << num << " is not a prime number." << endl; // exit the program
     }
     return 0;
 }

5f9b86df00f5f3a1e24203c632587b8a739669bb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-11-21 21:10:47 +0530

    add q 71

---
+++ basic problems/q71.cpp
@@ -0,0 +1,3 @@
+/*Write a C++ program which reads a sequence of integers and prints mode values of the sequence.
+The number of integers is greater than or equals to 1 and less than or equals to 100. Go to the editor
+Note: The mode of a set of data values is the value that appears most often*/
